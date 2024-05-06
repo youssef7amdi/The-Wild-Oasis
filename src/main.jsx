55b5ba763f8246efa62b25784a3420d18586ffc9
Youssef <youssef7amdi@gmail.com>
2024-05-06 11:19:27 +0300

    testing routing errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback.jsx";
+// import { ErrorBoundary } from "react-error-boundary";
+// import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary
+    {/* <ErrorBoundary
       FallbackComponent={ErrorFallback}
       onReset={() => window.location.replace("/")}
-    >
+    > */}
       <App />
-    </ErrorBoundary>
+    {/* </ErrorBoundary> */}
   </React.StrictMode>
 );
